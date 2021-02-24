c0a0b1c3aadff77fd687f64c6e595357e6b17d28
Intraraksa <intraraksa@gmail.com>
2021-02-24 16:07:04 +0700

    define load weight file func

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from tensorflow.keras.preprocessing.image import img_to_array,load_img
 from tempfile import NamedTemporaryFile
 from PIL import Image
 import numpy as np
-import os
 
 @st.cache
 def load_image(file):
@@ -15,11 +14,24 @@ def load_image(file):
     img = np.expand_dims(img,axis=0)
     return img
 
+## Predict model function
+def predict(image):
+    model = load_model('weight_file/defect.h5')
+    st.write(model.summary())
+
+
 def main():
     st.header("Flaw Detection")
+    
     st.markdown("This is the Defect detection program for classify front's flaw from images")
     with st.beta_expander(label="Show image sample"):
-        st.image('rose.jpg')
+        choose_sample = st.selectbox('Select sample part', ["Not show sample","ok sample","NG sample"])
+        if choose_sample == "ok sample":
+            st.image('util/cast_ok_0_8.jpeg')
+        elif choose_sample == "NG sample":
+            col1, col2 = st.beta_columns(2)
+            col1.image('util/cast_def_0_68.jpeg')
+            col2.image('util/cast_def_0_355.jpeg')
     uploaded = st.file_uploader("Upload image",type=['png','jpeg','jpg'])
     if uploaded is not None:
         st.image(Image.open(uploaded))
